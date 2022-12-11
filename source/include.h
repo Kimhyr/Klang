ff0c9a92f134617858f894109d8db4a9550c3bbd
Kimhyr <kingemhyr7210@gmail.com>
2022-12-11 17:36:21 -0500

    sdf

---
+++ source/include.h
@@ -0,0 +1,12 @@
+#ifndef  KPLC_INCLUDE_H
+#define  KPLC_INCLUDE_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+enum struct result {
+    failure = -1,
+    success,
+};
+
+#endif // KPLC_INCLUDE_H
